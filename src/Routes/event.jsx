807f0863d8571d52923f99875577e87e7190bf9f
Shivam Soni <104953953+Mrshivam007@users.noreply.github.com>
2023-12-01 23:30:35 +0530

    update

--- src/Routes/event.jsx
+++ src/Routes/event.jsx
@@ -2,14 +2,22 @@ import { Route, Router, Routes, Navigate } from "react-router-dom";
 import { Home, ListEvents, MyEventDetails, SponsorContentC, SponsorEvents } from "../pages";
 import ListeventsForm from "../pages/Create_Event/ListeventsForm";
 import CCForm from "../pages/Create_Content/CCForm";
+import EventHome from "../pages/Event/Home/EventHome";
+import EventCategories from "../pages/Event/EventCategories/EventCategories";
+import LiveEvent from "../pages/Event/EventCategories/LiveEvent";
+import UpcomingEvent from "../pages/Event/EventCategories/UpcomingEvent";
+import PreviousEvent from "../pages/Event/EventCategories/PreviousEvent";
 
 const EventRoutes = () => {
     console.log("Event Page");
     return (
         <>
             <Routes>
-                <Route path="/" element={<ListEvents />} />
-                <Route path="/events" element={<SponsorEvents />} />
+                <Route path="/" element={<EventHome />} />
+                <Route path="/events" element={<EventCategories />} />
+                <Route path="/events/live_event" element={<LiveEvent />} />
+                <Route path="/events/upcoming_event" element={<UpcomingEvent />} />
+                <Route path="/events/previous_event" element={<PreviousEvent />} />
                 <Route path="/myevent-details" element={<MyEventDetails />} />
                 <Route path="/cc" element={<SponsorContentC />} />
                 <Route path="/create_event" element={<ListeventsForm />} />
---
+++ src/pages/Event/EventCategories/PreviousEvent.jsx
@@ -0,0 +1,68 @@
+import React, {useEffect} from "react";
+import {
+  NavBar,
+  EventsHeader,
+  SponserE,
+  Footer,
+  MobileCards,
+} from "../../../components";
+import bgimage from "../../../assets/img/circle-bg.png";
+import spevents from "../../../assets/img/sponsor_events-logo.png";
+import { EventsCards, EventsPageCards } from "../../../data/data";
+import { fetchEvent } from "../../../redux/actions/eventAction";
+import { useDispatch, useSelector } from "react-redux"
+const PreviousEvent = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scrolls to the top of the page on component mount
+  }, []);
+
+  const dispatch = useDispatch();
+  const eventDetails = useSelector(state => state.event)
+  useEffect(() => {
+    dispatch(fetchEvent())
+  },[])
+
+  console.log("dynamic data",eventDetails.eventDetails);
+  console.log("static data",EventsCards);
+
+  return (
+    <>
+      <div
+        className="events-bg"
+        style={{
+          width: "100%",
+          height: "auto",
+          backgroundImage: `url(${bgimage})`,
+        }}
+      >
+        <NavBar />
+        <div className="events-page-desktop">
+          <EventsHeader title={"Previous Events"} logo={spevents} />
+          <SponserE cardData={eventDetails.eventDetails} line={"Previous Events"} />
+          {/* <SponserE cardData={EventsCards} line={"Concerts"} />
+          <SponserE cardData={EventsCards} line={"Promotional Events"} />
+          <SponserE cardData={EventsCards} line={"Sports Events"} />
+          <SponserE cardData={EventsCards} line={"Comedy Shows"} />
+          <SponserE cardData={EventsCards} line={"Motivational Events"} />
+          <SponserE cardData={EventsCards} line={"Reality Shows"} /> */}
+        </div>
+        <div className="events-page-mobile">
+          <MobileCards line={"Previous Events"} cardData={eventDetails.eventDetails} />
+          <div
+            className="btn d-block text-white font-weight-bolder"
+            style={{
+              margin: "5%",
+              borderRadius: "10px",
+              backgroundColor: "rgb(0, 78, 169)",
+            }}
+          >
+            Load More
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default PreviousEvent;
