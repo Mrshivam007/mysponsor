323b9ab4de5aadb6d32b3d344a921213bb7b94b2
amarmishra03 <109795692+amarmishra03@users.noreply.github.com>
2024-02-09 18:27:06 +0530

    url change

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,4 +1,4 @@
-const apiurl = "http://127.0.0.1:8000"
-// const apiurl = "http://13.233.151.159"
+// const apiurl = "http://127.0.0.1:8000"
+const apiurl = "http://13.233.151.159"
 
 export default apiurl;
